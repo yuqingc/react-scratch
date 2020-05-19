c1007088d4563a7b4fdb645576e60ea335938126
Yuqing CHEN <mr.chenyuqing@live.com>
2020-05-19 10:34:26 +0800

    chore:add eslint plugin for checking react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     'extends': [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
     ],
     'globals': {
         'Atomics': 'readonly',
